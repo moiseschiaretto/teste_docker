b0755b80b1a81b70a665fa3b16293b2465cc8eb4
Moisés Chiaretto <84775466+moiseschiaretto@users.noreply.github.com>
2024-09-11 13:10:40 -0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-    testEnvironment: 'node',
-    verbose: true,
-    roots: ['<rootDir>/__tests__'],
-    reporters: [
-      'default',
-      ['jest-html-reporter', {
-        pageTitle: "Test Report - Collection of API reqres.in",
-        outputPath: './test-report/report.html',
-        expand: true,
-        includeFailureMsg: true,
-      }],
-    ],
-  };
- 
+  testEnvironment: 'node',
+  // verbose: false, permite exibir o Console Log no report.html
+  verbose: false,
+  roots: ['<rootDir>/__tests__'],
+  reporters: [
+    'default',
+    ['jest-html-reporter', {
+      pageTitle: "Test Report - API Porto - Análise de Crédito.",
+      // outputPath: './report/report.html',
+      outputPath: process.env.REPORT_PATH || './report/default.html',
+      expand: true,
+      includeFailureMsg: true,
+      includeConsoleLog: true
+    }],
+  ],
+};
